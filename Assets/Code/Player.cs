c4315f074e63689709ce7de0bd5b017995481c9e
AimanHQ <haqimboy94@gmail.com>
2024-07-11 18:12:30 +0800

    Add Animation
    -new animation
    -camera now follow player

--- Assets/Code/Player.cs
+++ Assets/Code/Player.cs
@@ -7,13 +7,15 @@ public class Player : MonoBehaviour
     public float speed;
     float AxisH;
     float AxisV;
+    bool DownWalk;
 
     Vector3 moveVec;
+    Animator anim;
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        anim = GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
@@ -21,9 +23,17 @@ public class Player : MonoBehaviour
     {
         AxisH = Input.GetAxisRaw("Horizontal");
         AxisV = Input.GetAxisRaw("Vertical");
+        DownWalk =Input.GetButton("Walk");
 
         moveVec = new Vector3(AxisH, 0, AxisV).normalized;
 
-        transform.position += moveVec * speed * Time.deltaTime;
+        transform.position += moveVec * speed * (DownWalk ? 0.3f : 1f) * Time.deltaTime;
+
+
+        anim.SetBool("IsRun", moveVec != Vector3.zero);
+        anim.SetBool("IsWalk", DownWalk);
+
+        transform.LookAt(transform.position + moveVec);
+
     }
 }
