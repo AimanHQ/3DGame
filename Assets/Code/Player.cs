b733ebb4407ee28bfd57e9160b7bb9862c2f117b
AimanHQ <haqimboy94@gmail.com>
2024-08-07 18:52:03 +0800

    Update movement
    Add jump , change platform 45 degrees in rotation

--- Assets/Code/Player.cs
+++ Assets/Code/Player.cs
@@ -8,6 +8,8 @@ public class Player : MonoBehaviour
     float AxisH;
     float AxisV;
     bool DownWalk;
+    bool jump;
+    Rigidbody rb;
 
     Vector3 moveVec;
     Animator anim;
@@ -15,16 +17,29 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Awake()
     {
+        rb = GetComponent<Rigidbody>();
         anim = GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        GetInput();
+        Move();
+        Turn();
+        Jump();
+    }
+
+    void GetInput() 
+    {
         AxisH = Input.GetAxisRaw("Vertical");
         AxisV = Input.GetAxisRaw("Horizontal");
-        DownWalk =Input.GetButton("Walk");
+        DownWalk =Input.GetButton("Walk");  
+        jump = Input.GetButtonDown("Jump");      
+    }
 
+    void Move()
+    {
         moveVec = new Vector3(AxisH, 0, AxisV).normalized;
 
         transform.position += moveVec * speed * (DownWalk ? 0.3f : 1f) * Time.deltaTime;
@@ -32,8 +47,17 @@ public class Player : MonoBehaviour
 
         anim.SetBool("IsRun", moveVec != Vector3.zero);
         anim.SetBool("IsWalk", DownWalk);
+    }
 
+    void Turn()
+    {
         transform.LookAt(transform.position + moveVec);
+    }
 
+    void Jump()
+    {
+        if (jump) {
+            rb.AddForce(Vector3.up * 15 , ForceMode.Impulse);
+        }
     }
 }
